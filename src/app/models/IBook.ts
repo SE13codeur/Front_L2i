771c9daf0f6beb27f352f5907b58f45f57c6e048
SE13 <sebastien@attestis.com>
2023-03-09 08:49:17 +0100

    added model of a book

---
+++ src/app/models/IBook.ts
@@ -0,0 +1,25 @@
+export interface IBook {
+  id?: number;
+  title?: string;
+  subtitle?: string;
+  authors?: string;
+  publisher?: string;
+  isbn13?: string;
+  pages?: string;
+  year?: number;
+  description?: string;
+  price?: Float32Array;
+  url?: string;
+  image?: string;
+  category?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  regular_price?: number;
+  sold_price?: number;
+  isSolded?: boolean;
+  isNewCollection?: boolean;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
