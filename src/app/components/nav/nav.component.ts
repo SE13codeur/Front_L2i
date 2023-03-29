b37a5d6b8f9c5abdbd16c09c34f7459760ceba02
SE13 <sebastien@attestis.com>
2023-03-29 12:53:39 +0200

    implemented view of cart with cart and alert cart

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor() {}
+  totalItems = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.getCartItemCount().subscribe((count) => {
+      this.totalItems = count;
+    });
+  }
 }
