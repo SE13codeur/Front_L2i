7973b41626a7d94e3a08d35ab7e439286d3ce8a6
SE13 <sebastien@attestis.com>
2023-03-29 16:21:40 +0200

    updated models for cart

---
+++ src/app/models/ICartItem.ts
@@ -0,0 +1,10 @@
+import { BehaviorSubject } from 'rxjs';
+
+export interface ICartItem {
+  id: number;
+  isbn13: string;
+  title: string;
+  price: number;
+  quantity$: BehaviorSubject<number>;
+  description?: string;
+}
