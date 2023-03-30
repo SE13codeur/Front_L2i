b66aa36f291764429f14a8a520ba76487509eb42
SE13 <sebastien@attestis.com>
2023-03-30 15:43:53 +0200

    refactored nav bar for aside bar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-  totalItems = 0;
-
-  constructor(private cartService: CartService) {}
-
-  ngOnInit() {
-    this.cartService.getCartItemCount().subscribe((count) => {
-      this.totalItems = count;
-    });
-  }
-}
+export class NavComponent {}
