d0ea6c7f4fac33468915d17fa79b23014e460d7a
SE13 <sebastien@attestis.com>
2023-04-10 19:08:37 +0200

    tested .gitignore

--- src/app/services/meilisearch.service.ts
+++ src/app/services/meilisearch.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '@env/environment';
+import { environment } from '@env/environments.dev';
 import { IMeilisearchResult } from '@m/IMeilisearchItem';
 import { map } from 'rxjs/operators';
 
