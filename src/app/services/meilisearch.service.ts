acdcd026a37599f580f27c59e14e9dbbae601d0b
SE13 <sebastien@attestis.com>
2023-04-10 18:32:59 +0200

    implemented search bar with Meilisearch

---
+++ src/app/services/meilisearch.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from '@env/environment';
+import { IMeilisearchResult } from '@m/IMeilisearchItem';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MeiliSearchService {
+  private readonly baseUrl: string;
+  private searchResults = new BehaviorSubject<IMeilisearchResult[]>([]);
+
+  constructor(private readonly http: HttpClient) {
+    this.baseUrl = `${environment.meiliSearchApiUrl}/indexes/items/search`;
+  }
+
+  search(query: string): Observable<IMeilisearchResult[]> {
+    return this.http
+      .get<{ hits: IMeilisearchResult[] }>(this.baseUrl, {
+        params: { q: query },
+      })
+      .pipe(map((response) => response.hits));
+  }
+
+  getSearchResults(): Observable<IMeilisearchResult[]> {
+    return this.searchResults.asObservable();
+  }
+}
