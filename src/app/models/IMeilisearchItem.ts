1b87e63044d6590bec861488e8d1c7c3580d1bad
SE13 <sebastien@attestis.com>
2023-04-14 15:42:38 +0200

    modified items models for Meilisearch API

--- src/app/models/IMeilisearchItem.ts
+++ src/app/models/IMeilisearchItem.ts
@@ -1,57 +1,40 @@
-export interface ICategoryItem {
-  id: number;
-  name: string;
-  parentId?: number;
-}
-
-export interface MeiliSearchModel {
-  meiliSearchId: string;
-  indexUid: string;
-}
-
-export interface Item extends MeiliSearchModel {
-  id: number;
-  book: IBook;
-  imageUrl: string;
-  description: string;
-  category: ICategoryItem;
-  regularPrice: number;
-  inStock: boolean;
-  isNewCollection: boolean;
-  language: string;
-  totalSales: number;
-  createdAt: Date;
-  updatedAt: Date;
-  indexUid: string;
-}
-
-export interface IBook extends Item {
-  id: number;
+export interface IMeilisearchItem {
+  uid: string;
   isbn13: string;
+  imageUrl: string;
   title: string;
   subtitle: string;
-  summary: string;
-  authors: IAuthor[];
-  editor: IEditor;
-  category: ICategoryItem;
+  description: string;
+  regularPrice: number;
+  rating?: number;
+  inStock: boolean;
+  quantityInStock: number;
+  totalSales: number;
+  authors?: IAuthor[];
+  editor?: IEditor;
+  categorie: ICategoryItem;
   pages: string;
   year: string;
-  version: number;
+  language?: string;
+  version?: number;
+  newCollection: boolean;
 }
 
 export interface IAuthor {
   id: number;
   firstName: string;
   lastName: string;
-  books?: IBook[];
+  books?: IMeilisearchItem[];
 }
 
 export interface IEditor {
   id: number;
   name: string;
-  books?: IBook[];
+  books?: IMeilisearchItem[];
 }
 
-export interface IMeilisearchResult extends ICategoryItem, Item {
-  uid: string;
+export interface ICategoryItem {
+  id: number;
+  name: string;
+  parentId?: number;
 }
