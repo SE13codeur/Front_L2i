678570fc9bcb4c89e7a4b37a5e1346a7de09c157
SE13 <sebastien@attestis.com>
2023-04-18 19:02:47 +0200

    fixed bug for updating price range

--- src/app/components/section/item/list-item/list-item.component.ts
+++ src/app/components/section/item/list-item/list-item.component.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 import { IMeilisearchItem } from '@m/IMeilisearchItem';
 import { MeiliSearchService } from '@s/meilisearch.service';
+import { FiltersService } from '@s/filters.service';
 
 @Component({
   selector: 'app-list-item',
@@ -12,13 +13,15 @@ import { MeiliSearchService } from '@s/meilisearch.service';
 })
 export class ListItemComponent implements OnInit, OnDestroy {
   itemList$ = new BehaviorSubject<IMeilisearchItem[]>([]);
+  private originalItemList: IMeilisearchItem[] = [];
 
   private readonly destroy$ = new Subject<void>();
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private meiliSearchService: MeiliSearchService
+    private meiliSearchService: MeiliSearchService,
+    private filtersService: FiltersService
   ) {}
 
   ngOnInit() {
@@ -30,14 +33,21 @@ export class ListItemComponent implements OnInit, OnDestroy {
           this.meiliSearchService
             .updatedSearch(query)
             .subscribe((searchResults) => {
-              this.itemList$.next(searchResults);
+              this.originalItemList = searchResults;
+              this.applyFilters();
             });
         } else {
           this.meiliSearchService.getAllItems().subscribe((allBooks) => {
-            this.itemList$.next(allBooks);
+            this.originalItemList = allBooks;
+            this.applyFilters();
           });
         }
       });
+
+    this.filtersService.subscribeToAllFilters(
+      () => this.applyFilters(),
+      this.destroy$
+    );
   }
 
   ngOnDestroy(): void {
@@ -45,10 +55,52 @@ export class ListItemComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
+  applyFilters() {
+    // Apply all filters to the originalItemList and update itemList$
+    let filteredItems = this.originalItemList;
+
+    // Apply categories filter
+    const categories = this.filtersService.categoriesSource.getValue();
+    if (categories.length > 0) {
+      filteredItems = filteredItems.filter((item) =>
+        categories.includes(item.category.name)
+      );
+    }
+
+    // Apply authors filter
+    const authors = this.filtersService.authorsSource.getValue();
+    if (authors.length > 0) {
+      filteredItems = filteredItems.filter((item) =>
+        item.authors?.some((author) =>
+          authors.includes(`${author.firstName} ${author.lastName}`)
+        )
+      );
+    }
+
+    // Apply priceMin and priceMax filters
+    const priceMin = this.filtersService.priceMinSource.getValue();
+    const priceMax = this.filtersService.priceMaxSource.getValue();
+    filteredItems = filteredItems.filter(
+      (item) => item.regularPrice >= priceMin && item.regularPrice <= priceMax
+    );
+
+    // Apply ratings filter
+    const ratings = this.filtersService.ratingsSource.getValue();
+    if (ratings.length > 0) {
+      filteredItems = filteredItems.filter((item) =>
+        ratings.includes(item.rating ?? 0)
+      );
+    }
+
+    // Update itemList$ with filteredItems
+    this.itemList$.next(filteredItems);
+  }
+
   openItemDetails(item: IMeilisearchItem) {
     this.router.navigate(['/items', item.id]);
   }
 
+  //TODO
   addToFavorites(item: IMeilisearchItem, event: Event) {
     console.log('Item added to favorites:', item);
     event.stopPropagation();
