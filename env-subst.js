3fe69b1a36c52c14440cc71f67df7a0c3c792aa6
SE13 <sebastien@attestis.com>
2023-05-09 20:59:10 +0200

    updated env variables for Vercel deployment with dotenv

---
+++ env-subst.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+const envFile = fs.readFileSync('.env.production', 'utf-8');
+const envConfig = dotenv.parse(envFile);
+
+const prodConfigFile = fs.readFileSync('src/environments/environment.prod.ts', 'utf-8');
+const newConfigFile = prodConfigFile
+  .replace('${MEILISEARCH_API_KEY}', envConfig.MEILISEARCH_API_KEY)
+  .replace('${MEILISEARCH_API_URL}', envConfig.MEILISEARCH_API_URL)
+  .replace('${API_URL}', envConfig.API_URL);
+
+fs.writeFileSync('src/environments/environment.prod.ts', newConfigFile);
