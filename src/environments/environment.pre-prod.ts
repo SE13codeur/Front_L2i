58bbb55a7580d47f3a9f3705beeded5ec0a4bbae
SEBattestis <94559855+SEBattestis@users.noreply.github.com>
2023-05-09 20:36:51 +0200

    Delete environment.pre-prod.ts

--- src/environments/environment.pre-prod.ts
+++
@@ -1,6 +0,0 @@
-export const environmentPreProd = {
-  production: true,
-  meiliSearchApiKey: '3ea36fbcd1d515555809bb02078ba8d3265a6abc',
-  meiliSearchApiUrl: 'https://ms-83b035fcd6c1-3155.fra.meilisearch.io',
-  apiUrl: 'http://localhost:909/ws/dev/api',
-};
