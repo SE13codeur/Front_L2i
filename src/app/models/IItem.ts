6048f69be8527d1c3bc94a7be7c9be90b4bc7a7e
SE13 <sebastien@attestis.com>
2023-05-24 13:15:00 +0200

    displayed paginated books without Meilisearch

--- src/app/models/IItem.ts
+++ src/app/models/IItem.ts
@@ -1,22 +1,22 @@
 export default interface IItem {
   id: string;
-  isbn13?: string;
+  isbn13: string;
   imageUrl: string;
   title: string;
   subtitle: string;
   description: string;
   regularPrice: number;
-  rating?: number;
+  rating: number;
   inStock: boolean;
   quantityInStock: number;
   totalSales: number;
-  authors?: IAuthor[];
-  editor?: IEditor;
+  authors: IAuthor[];
+  editor: IEditor;
   category: ICategoryItem;
   pages: string;
   year: string;
-  language?: string;
-  version?: number;
+  language: string;
+  version: number;
   newCollection: boolean;
 }
 
