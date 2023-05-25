f0116f462a529320402e9ac198e28055326b7f0a
SE13 <sebastien@attestis.com>
2023-05-25 20:04:32 +0200

    fixed bug of type for category

--- src/app/components/admin/admin-item/admin-item.component.ts
+++ src/app/components/admin/admin-item/admin-item.component.ts
@@ -28,7 +28,7 @@ export class AdminItemComponent implements OnInit {
   editors: IEditor[] = [];
   categories: ICategoryItem[] = [];
   isSubmitting = false;
-
+  isAddRouting = !!this.route.snapshot.paramMap.get('id') ? false : true;
   constructor(
     private fb: FormBuilder,
     private adminItemService: AdminItemService,
@@ -51,12 +51,15 @@ export class AdminItemComponent implements OnInit {
       subtitle: ['', Validators.required],
       description: ['', Validators.required],
       regularPrice: ['', [Validators.required, Validators.min(0)]],
-      inStock: [false],
+      inStock: [true],
       quantityInStock: ['', [Validators.required, Validators.min(1)]],
-      rating: ['', [Validators.required]],
+      rating: [
+        this.isAddRouting ? 5 : this.item?.rating,
+        [Validators.required],
+      ],
       authors: this.fb.array([], Validators.required),
-      editor: [[], Validators.required],
-      category: [[], Validators.required],
+      editor: ['', Validators.required],
+      category: ['', Validators.required],
       pages: ['', [Validators.required, Validators.min(1)]],
       year: [
         '',
@@ -66,7 +69,7 @@ export class AdminItemComponent implements OnInit {
           Validators.max(new Date().getFullYear()),
         ],
       ],
-      language: ['', Validators.required],
+      language: [this.isAddRouting ? 'french' : this.item?.language],
       version: ['', Validators.required],
       newCollection: [false],
     });
@@ -97,10 +100,6 @@ export class AdminItemComponent implements OnInit {
       this.itemService.getItemById(this.itemId).subscribe({
         next: (item: IItem) => {
           this.item = item;
-
-          const authorsIds = item.authors.map((author) => author.id);
-
-          // Update form controls
 
           // Update form controls
           this.itemForm.patchValue({
@@ -171,17 +170,22 @@ export class AdminItemComponent implements OnInit {
       )
       .filter((v: string | null) => v !== null);
 
-    itemData.editor = this.editors.find(
-      (editor) => editor.id === itemData.editor
-    );
     itemData.authors = this.authors.filter((author) =>
       itemData.authors.includes(author.id)
     );
-    itemData.category = this.categories.find(
-      (category) => category.id === itemData.category
+
+    let editorObject = this.editors.find(
+      (editor) => editor.id == itemData.editor
     );
-    itemData.rating = this.item?.rating;
-    itemData.language = this.item?.language;
+    itemData.editor = editorObject;
+
+    let categoryObject = this.categories.find(
+      (category) => category.id == itemData.category
+    );
+    itemData.category = categoryObject;
+
+    itemData.rating = this.isAddRouting ? 5 : this.item?.rating;
+    itemData.language = this.isAddRouting ? 'french' : this.item?.language;
 
     return itemData;
   }
