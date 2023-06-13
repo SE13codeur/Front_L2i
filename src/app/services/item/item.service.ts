9483e06b25b442e8c43cc7caf9d98b473ea7a352
SE13 <sebastien@attestis.com>
2023-06-13 07:23:14 +0200

    modified env for prod

--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environmentPreProd as environment } from '@env/environment.pre-prod';
+import { environmentProd as environment } from '@env/environment.prod';
 import { IItem } from '@models/index';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
