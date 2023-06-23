7f46e4d414d79bf86ac3d49db2b43f664e033ad3
SE13 <sebastien@attestis.com>
2023-06-23 15:18:52 +0200

    moved order components into section

--- src/app/components/shared/index.ts
+++ src/app/components/shared/index.ts
@@ -1,5 +1,4 @@
 export * from './aside-cart';
 export * from './cart-item-quantity/cart-item-quantity.component';
 export * from './nav';
-export * from './order';
 export * from './aside-account-user';
