13fa96f5f3f590909765277e400cf4114b0f8baf
SE13 <sebastien@attestis.com>
2023-06-29 20:07:58 +0200

    displayed all users from admin

--- src/app/pages/user-account/admin-user-page/admin-user-page.component.ts
+++ src/app/pages/user-account/admin-user-page/admin-user-page.component.ts
@@ -1,5 +1,9 @@
+import { AdminUserService } from '@services/index';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IUser } from '@models/user';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from '@auth-s/auth.service';
 
 @Component({
   selector: 'app-admin-user-page',
@@ -7,9 +11,35 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-user-page.component.css'],
 })
 export class AdminUserPageComponent implements OnInit {
-  constructor(private router: Router) {}
+  users$ = new BehaviorSubject<IUser[]>([]);
+  user: IUser | null = null;
+  isAdmin = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private adminUserService: AdminUserService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      if (this.user && !this.isAdmin) {
+        this.getAllUsers(this.user.id);
+      }
+    });
+  }
+
+  getAllUsers(userId: number): void {
+    this.adminUserService.getAllUsers().subscribe({
+      next: (users) => {
+        this.users$.next(users);
+      },
+      error: (error) => {
+        console.error('Error while fetching users:', error);
+      },
+    });
+  }
 
   goToAdminUserForm() {
     this.router.navigate(['/admin/users']);
